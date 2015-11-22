3a41b46de571882574e6e5b733126462042e6ead
tarlepp <tarmo.leppanen@kase.fi>
2015-11-22 12:31:24 +0200

    Added tslint task.

---
+++ tools/tasks/tslint.ts
@@ -0,0 +1,23 @@
+// Dependencies
+import {join} from 'path';
+import {APP_SRC, TOOLS_DIR} from '../config';
+
+// Actual task implementation
+export = function tslint(gulp, plugins) {
+  return function () {
+    let src = [
+      join(APP_SRC, '**/*.ts'),
+      join(TOOLS_DIR, '**/*.ts'),
+      '!' + join(APP_SRC, '**/*.d.ts'),
+      '!' + join(TOOLS_DIR, '**/*.d.ts')
+    ];
+
+    return gulp.src(src)
+      .pipe(plugins.tslint())
+      .pipe(plugins.tslint.report(plugins.tslintStylish, {
+        emitError: false,
+        sort: true,
+        bell: true
+      }));
+  };
+};
