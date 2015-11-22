df583024782b570f4fb14798147b6d67fc7442e6
tarlepp <tarmo.leppanen@kase.fi>
2015-11-22 13:22:54 +0200

    Added template locals.

---
+++ tools/utils/template-locals.ts
@@ -0,0 +1,12 @@
+// Dependencies
+import {APP_BASE, APP_TITLE, SYSTEM_CONFIG, VERSION} from '../config';
+
+// TODO: Add an interface to register more template locals.
+export function templateLocals() {
+  return {
+    APP_BASE,
+    APP_TITLE,
+    SYSTEM_CONFIG,
+    VERSION
+  };
+}
