b97cafbc38ab17b62ab87e87561bcba7e9ba4281
tarlepp <tarmo.leppanen@kase.fi>
2015-11-22 12:44:11 +0200

    Added sass task.

---
+++ tools/tasks/build.sass.dev.ts
@@ -0,0 +1,12 @@
+// Dependencies
+import {join} from 'path';
+import {APP_SRC} from '../config';
+
+// Actual task implementation
+export = function buildSassDev(gulp, plugins, option) {
+  return function() {
+    return gulp.src(join(APP_SRC, '**', '*.scss'))
+      .pipe(plugins.sass().on('error', plugins.sass.logError))
+      .pipe(gulp.dest(APP_SRC));
+  };
+}
