76973c7682959e9ac715dcf49024a77dd86c6e7d
tarlepp <tarmo.leppanen@kase.fi>
2015-11-24 23:08:08 +0200

    Fixed sass/scss task

--- tools/tasks/build.sass.dev.ts
+++ tools/tasks/build.sass.dev.ts
@@ -1,12 +1,12 @@
 // Dependencies
 import {join} from 'path';
-import {APP_SRC} from '../config';
+import {APP_SRC, APP_DEST} from '../config';
 
 // Actual task implementation
 export = function buildSassDev(gulp, plugins, option) {
   return function() {
     return gulp.src(join(APP_SRC, '**', '*.scss'))
       .pipe(plugins.sass().on('error', plugins.sass.logError))
-      .pipe(gulp.dest(APP_SRC));
+      .pipe(gulp.dest(APP_DEST));
   };
 }
