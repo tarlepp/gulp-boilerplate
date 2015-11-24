036ab649769dc2ad58be1933268404f9cc44c759
tarlepp <tarmo.leppanen@kase.fi>
2015-11-24 22:52:16 +0200

    Added base structure of actual SPA application.

---
+++ app/js/test.js
@@ -0,0 +1,8 @@
+(function() {
+  console.log('this is a plain old JavaScript example file, see source /app/js/test.js');
+
+  var list = [1, 2, 3];
+  var list2 = list.map(function(number) { return number * 2; });
+
+  console.log(list, list2);
+})();
---
+++ app/ts/test.ts
@@ -0,0 +1,6 @@
+console.log('this is a TypeScript example file, see source /app/ts/test.ts');
+
+let list: number[] = [1, 2, 3];
+let list2 = list.map((number: number) => number * 2);
+
+console.log(list, list2);
