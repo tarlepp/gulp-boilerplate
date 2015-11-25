9ab546de8906c6ed92dea16d58b4c0baec891a26
tarlepp <tarmo.leppanen@kase.fi>
2015-11-25 20:18:42 +0200

    Added export of file-tools

--- tools/utils.ts
+++ tools/utils.ts
@@ -2,6 +2,7 @@ export * from './utils/template-injectables';
 export * from './utils/template-locals';
 export * from './utils/server';
 export * from './utils/tasks-tools';
+export * from './utils/file-tools';
 
 export function tsProjectFn(plugins) {
   return plugins.typescript.createProject('tsconfig.json', {
