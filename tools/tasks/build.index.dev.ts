f892925820fdd853d36fea824433efc52b7c813c
tarlepp <tarmo.leppanen@kase.fi>
2015-11-25 20:20:04 +0200

    Added injections of javascript and css files.

--- tools/tasks/build.index.dev.ts
+++ tools/tasks/build.index.dev.ts
@@ -1,29 +1,49 @@
 // Dependencies
 import {join} from 'path';
 import {APP_SRC, APP_DEST, DEV_DEPENDENCIES} from '../config';
-import {transformPath, templateLocals} from '../utils';
+import {cssFiles, jsFiles, transformPath, templateLocals} from '../utils';
 
+// Actual task implementation
 export = function buildIndexDev(gulp, plugins) {
   return function () {
     return gulp.src(join(APP_SRC, 'index.html'))
       // NOTE: There might be a way to pipe in loop.
       .pipe(inject('shims'))
       .pipe(inject())
+      .pipe(inject('', cssFiles()))
+      .pipe(inject('', jsFiles()))
       .pipe(plugins.template(templateLocals()))
       .pipe(gulp.dest(APP_DEST));
   };
 
+  /**
+   * Helper inject method to return stream with specified injectables.
+   *
+   * @param   {string}                  [name]
+   * @param   {NodeJS.ReadWriteStream}  [source]
+   * @returns {NodeJS.ReadWriteStream}
+   */
+  function inject(name?: string, source?: NodeJS.ReadWriteStream) {
+    // No source given so get default ones
+    if (!source) {
+      source = gulp.src(getInjectablesDependenciesRef(name), { read: false });
+    }
 
-  function inject(name?: string) {
-    return plugins.inject(gulp.src(getInjectablesDependenciesRef(name), { read: false }), {
+    return plugins.inject(source, {
       name,
       transform: transformPath(plugins, 'dev')
     });
   }
 
+  /**
+   * Helper method to get injectables dependencies (npm packages) from configuration file.
+   *
+   * @param   {string}  name
+   * @returns {string[]}
+   */
   function getInjectablesDependenciesRef(name?: string) {
     return DEV_DEPENDENCIES
       .filter(dep => dep['inject'] && dep['inject'] === (name || true))
       .map(dep => `${dep.dest}/${dep.src.split('/').pop()}`);
   }
-};
+}
