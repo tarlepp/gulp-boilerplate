770a64e17ff3b34455f2c10db77514024b6d7e7e
tarlepp <tarmo.leppanen@kase.fi>
2015-11-25 20:23:45 +0200

    Enabled serve.dev task

--- gulpfile.ts
+++ gulpfile.ts
@@ -9,20 +9,19 @@ loadTasks();
 
 /**
  * Below is all supported gulp main tasks. Note that these task will use sub-tasks to make all magic to work.
- * Following gulp task are supported:
- *  - gulp serve [--{environment}]
  *
- * Other gulp tasks (following are just 'sub' tasks
- *  - gulp clean
- *  - gulp clean.dist
- *  - gulp clean.test
- *  - gulp postinstall
+ *  gulp serve [--{environment}]
+ *
+ * Other gulp tasks are just individual tasks that main tasks uses. You can get full gulp task list just by typing
+ * following command in shell:
+ *
+ *  gulp --tasks-simple
  */
 
 // Clean tasks
-gulp.task('clean',       task('clean', 'all'));
-gulp.task('clean.dist',  task('clean', 'dist'));
-gulp.task('clean.test',  task('clean', 'test'));
+gulp.task('clean',      task('clean', 'all'));
+gulp.task('clean.dist', task('clean', 'dist'));
+gulp.task('clean.test', task('clean', 'test'));
 
 // Post install.
 gulp.task('postinstall', done =>
@@ -38,9 +37,11 @@ gulp.task('build.dev', done =>
   runSequence(
     'clean.dist',
     'tslint',
+    'build.deps',
     'build.sass.dev',
     'build.assets',
     'build.js.dev',
+    'build.ts.dev',
     'build.index.dev',
     done
   )
@@ -57,7 +58,7 @@ gulp.task('build.dev.watch', done =>
 // Serve each environment
 gulp.task('serve', done =>
   runSequence(
-    // `build.${ENV}`, this needs work still...
+    `build.${ENV}`,
     'server.start',
     'watch.serve',
     done
